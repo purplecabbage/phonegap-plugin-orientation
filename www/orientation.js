e4545638fe7dd68d80f343280c9697231f2044b8
Jesse MacFadyen <purplecabbage@gmail.com>
2016-03-11 12:43:31 -0800

    added orientation mask static values

--- www/orientation.js
+++ www/orientation.js
@@ -1,7 +1,14 @@
 
 
+
 module.exports = {
+	Orientations: {
+		'Portrait':1,
+		'PortraitUpsideDown':2,
+		'Landscape':4,
+		'LandscapeUpsideDown':8
+	},
 	setAllowedOrientations(orientationMask) {
-		cordova.exec(0,0,"PGOrientation","setAllowedOrientations",orientationMask);
+		cordova.exec(0,0,"PGOrientation","setAllowedOrientations",[orientationMask]);
 	}
 }
