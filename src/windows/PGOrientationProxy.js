8829ad54af30015cd0a874fb6a3aefecff90c901
Jesse MacFadyen <purplecabbage@gmail.com>
2016-03-15 00:17:07 -0700

    added actual implementation

--- src/windows/PGOrientationProxy.js
+++ src/windows/PGOrientationProxy.js
@@ -1,7 +1,38 @@
+
+    var DisplayInfo = Windows.Graphics.Display.DisplayInformation;
+    var Orientations = Windows.Graphics.Display.DisplayOrientations;
+   
 module.exports = {
-	setAllowedOrientations:function(win, fail, args) {
-		console.log("setAllowedOrientations proxy called with " + args);
-	}
+    setAllowedOrientations: function (win, fail, args) {
+        //console.log("setAllowedOrientations proxy called with " + args);
+
+        try {
+            var prefOrients = args[0];
+            var winPrefs = 0;
+
+            if (prefOrients & 1) { // UIInterfaceOrientationPortrait
+                winPrefs = winPrefs |  Orientations.portrait;
+            }
+            if (prefOrients & 2) { // UIInterfaceOrientationPortraitUpsideDown
+                winPrefs = winPrefs | Orientations.portraitFlipped;
+            }
+            if(prefOrients & 4) { // UIInterfaceOrientationLandscapeLeft
+                winPrefs = winPrefs | Orientations.landscape;
+            }
+            if (prefOrients & 8) { // UIInterfaceOrientationLandscapeRight
+                winPrefs = winPrefs | Orientations.landscapeFlipped;
+            }
+            setTimeout(function () {
+                DisplayInfo.autoRotationPreferences = winPrefs;
+                win();
+            }, 0);
+        }
+        catch (err) {
+            console.log("error :: " + err);
+            fail();
+        }
+        
+    }
 }
 
 require("cordova/exec/proxy").add("PGOrientation", module.exports);
